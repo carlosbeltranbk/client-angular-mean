c4057f3aa1da363ea4e1ea901ef4973b3579e075
Carlos Beltran <carlosbeltranbk>
2021-08-28 19:25:13 -0500

    Va configuardo para subir a cloud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+//componentes del proyecto
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+
+const routes: Routes = [
+  {path: '', component: ListarProductosComponent},
+  {path: 'crear-producto', component: CrearProductoComponent},
+  {path: 'editar-producto/:id', component: CrearProductoComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'} //esta se pone para validar que no se ingrese ninguna ruta mal, en caso de que ocurra, la redirecciona al inicio.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -0,0 +1,89 @@
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
+
+@Component({
+  selector: 'app-crear-producto',
+  templateUrl: './crear-producto.component.html',
+  styleUrls: ['./crear-producto.component.css']
+})
+export class CrearProductoComponent implements OnInit {
+
+  productoForm: FormGroup;
+  titulo = 'Crear producto';
+  id: string | null;
+
+  constructor(private fb: FormBuilder, 
+          private router: Router,
+          private toastr: ToastrService,
+          private _productoService: ProductoService,
+          private aRouter: ActivatedRoute) { 
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required]
+    })
+    this.id = this.aRouter.snapshot.paramMap.get('id');
+  }
+
+  ngOnInit(): void {
+    this.esEditar();
+  }
+
+  agregarProducto() {
+    console.log(this.productoForm);
+
+    const PRODUCTO: Producto = {
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value,
+    }
+
+    if (this.id !== null) {
+      //Vamos a editar el producto
+      this._productoService.editarProducto(this.id, PRODUCTO).subscribe(data =>{
+        this.toastr.info(':)!', 'Producto editado correctamente!');
+        this.router.navigate(['/']);
+      }, error =>{
+        console.log('Ocurrio un error');
+        console.log(error);
+        this.toastr.error('Ops!', 'Ocurrio un error!');
+        this.productoForm.reset();
+      })
+    }else{
+      // vamos a registrar el producto
+      console.log(PRODUCTO);
+      this._productoService.guardarProducto(PRODUCTO).subscribe(data =>{
+        this.toastr.success(':)!', 'Producto Registrado!');
+        this.router.navigate(['/']);
+      }, error =>{
+        console.log('Ocurrio un error');
+        console.log(error);
+        this.toastr.error('Ops!', 'Ocurrio un error!');
+        this.productoForm.reset();
+      })
+    }
+  }
+
+
+  esEditar(){
+    if (this.id !== null) {
+      this.titulo = 'Editar Producto';
+      this._productoService.obtenerProducto(this.id).subscribe(data =>{
+        this.productoForm.setValue({
+          producto: data.nombre,
+      categoria: data.categoria,
+      ubicacion: data.ubicacion,
+      precio: data.precio
+        })
+      })
+    } 
+  }
+
+}
